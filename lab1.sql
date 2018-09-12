432790363259b4890de27b29c70e7b4877a7fb88
Esken-VIA <32093578+Esken-VIA@users.noreply.github.com>
2018-09-12 11:59:08 +0600

    Add files via upload

---
+++ lab1.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE lab1;
+
+CREATE TABLE users(
+  id serial ,
+  firstname varchar(50) NOT NULL ,
+  lastname varchar(50) NOT NULL
+);
+
+ALTER TABLE users
+  ADD COLUMN isadmin VARCHAR(50);
+
+ALTER TABLE users
+  ALTER COLUMN isadmin DROP DEFAULT;
+
+ALTER TABLE users
+  ALTER isadmin TYPE bool USING isadmin::boolean;
+
+ALTER TABLE users
+  ALTER COLUMN isadmin SET DEFAULT FALSE;
+
+ALTER TABLE users
+  ADD CONSTRAINT primaryKey_tmp PRIMARY KEY (id);
+
+
+CREATE TABLE tasks(
+  id integer PRIMARY KEY ,
+  name varchar(50) NOT NULL ,
+  user_id integer NOT NULL
+);
+
+DROP TABLE tasks;
+
+DROP DATABASE lab1;
